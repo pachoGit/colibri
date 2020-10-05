d510b6a431ab480c6747e285b41eb272d5d9048d
Yangura <71035560+Yangura@users.noreply.github.com>
2020-10-04 19:55:52 -0500

    secciones

---
+++ app/Views/secciones/editar.php
@@ -0,0 +1,168 @@
+<?php
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+
+    //var_dump($_POST);
+    //var_dump($_FILES);die;
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => base_url()."/index.php/secciones/update/".$_POST["idSeccion"],
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "PUT",
+        CURLOPT_POSTFIELDS =>
+        "seccion=".$_POST["seccion"].
+        "&id_grado=".$_POST["id_grado"].
+        "&fechaCreacion=".$_POST["fechaCreacion"].
+        "&fechaElim=".$_POST["fechaElim"].
+        "&id_cliente=1",
+        CURLOPT_HTTPHEADER => array(
+            $_SESSION["auth"],
+            "Content-Type: application/x-www-form-urlencoded",
+                                    ),
+                                   ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+
+    // Puede que tengamos caracteres ocultos la final de la respuesta
+    $data = substr($response, 0, $_SESSION["tam"]);
+    $data = json_decode($data, true);
+    if ($data["Estado"] != 200)
+    {
+        var_dump($data);die;
+	}
+    // Redireccion
+    var_dump($data);
+    return redirect()->to(base_url()."/index.php/secciones/listar");;
+    
+}
+else
+{
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => base_url()."/index.php/secciones/show/".$id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            $_SESSION["auth"],
+                                    ),
+                                   ));
+    
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    // Puede que tengamos caracteres ocultos la final de la respuesta
+    $data = substr($response, 0, $_SESSION["tam"]);
+    $data = json_decode($data, true);
+
+    $casa = new App\Controllers\Casa();
+    $nmodulos = $casa->traerModulos();
+
+    $datos = ["perfil"  => $_SESSION["perfil"],                                                                                                         
+              "titulo"  => "SECCIONES",                                                                                                                   
+              "nombre"  => $_SESSION["nombres"],                                                                                                       
+              "modulos" => $nmodulos];
+
+    $casa->cargarCabeza($datos);
+
+    $data = $data["Detalles"];
+    $data = $data[0];
+
+}
+
+
+?>
+
+
+<!--main-container-part-->
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+    <div id="content">
+	<br><br>
+	<!--Action boxes-->
+	<div class="container-fluid">
+	    <!--End-Action boxes-->    
+
+	    <!--Chart-box-->    
+	    <div class="row-fluid">
+		<div class="widget-box">
+		    <div class="widget-title bg_lg">
+			<h3>Editar secciones</h3>
+		    </div>
+		    <div class="widget-content" >
+			
+			<form  method="post" /*action="<?php base_url().'/index.php/seciones/create'?>"*/ enctype="multipart/form-data" class="needs-validation" novalidate>
+			<input type="hidden" name="idSeccion" value="<?= $id; ?>">                                                                                                                                                             
+			    <div class="form-row">
+			    <div class="form-group col-md-4">
+				    <label for="id_grado">Seleccione Grado</label>
+				    <select id="id_grado" name="id_grado" class="form-control" required>
+					<?php foreach ($grados as $grado): ?>
+					    <option value="<?= $grado["idGrado"]?>" <?php if ($grado["grado"] == $mi_grado) { echo "selected"; }?> > <?= $grado["grado"]; ?></option>
+					    <?php endforeach; ?> 
+				    </select>
+				</div>
+				<div class="form-group col-md-6">
+				    <label for="Seccion">Seccion</label>
+				    <input type="text" name="seccion" value="<?= $data["seccion"]; ?>" class="form-control" id="seccion" required>
+				    <div class="valid-feedback">
+					Esto est&aacute; bien
+				    </div>
+				    <div class="invalid-feedback">
+					Ingrese su nombre
+				    </div>
+				</div>
+			    </div>
+
+			    <div class="form-row">
+				<div class="form-group col-md-6">
+				    <label for="fechaCreacion">Fecha Creación</label>
+				    <input type="date" name="fechaCreacion" class="form-control" id="fechaCreacion" value="<?= $data["fechaCreacion"]; ?>" disable>
+				    <div class="valid-feedback">
+					Esto est&aacute; bien
+				    </div>
+				    <div class="invalid-feedback">
+					Ingrese un correo electr&oacute;nico valido
+				    </div>
+				</div>
+
+				<div class="form-group col-md-6">
+				    <label for="contra">Fecha Eliminación </label>
+				    <input type="date" class="form-control" name="fechaElim" id="fechaElim" value="<?= $data["fechaElim"]; ?>" disable>
+				    <div class="invalid-feedback">
+					Rellene este campo
+				    </div>
+				</div>
+			    </div>
+			    <button type="submit" class="btn btn-primary">Aceptar</button>
+                            <a href="<?= base_url().'/index.php/secciones/listar'; ?>" class="btn btn-danger"> Cancelar </a>
+			</form>
+			
+		    </div>
+
+		</div>
+	    </div>
+	</div>
+	<!--End-Chart-box--> 
+	<hr/>
+    </div>
+</main>
+</div>
+</div>
+
+<?php echo view("comun/pie"); ?>
---
+++ app/Views/secciones/eliminar.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => base_url()."/index.php/secciones/delete/".$id,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "DELETE",
+  CURLOPT_HTTPHEADER => array(
+      $_SESSION["auth"],
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+
+
+?>
